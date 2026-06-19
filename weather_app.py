21e4cd4db522691faef04d1c2ad13daf28057432
rutujaingole8767 <rutujaingole8767@gmail.com>
2026-06-19 18:57:18 +0530

    Add files via upload

---
+++ weather_app.py
@@ -0,0 +1,8 @@
+city = input("Enter city name: ")
+
+print("\n----- Weather Report -----")
+print("City:", city)
+print("Temperature: 30°C")
+print("Humidity: 65%")
+print("Condition: Sunny")
+print("--------------------------")
